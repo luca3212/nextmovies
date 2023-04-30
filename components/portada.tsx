a5e910eed2451800f687262cd15f856b8053bcb4
luca <luca-93@live.com.ar>
2023-04-30 17:46:54 -0300

    portada automatica

--- components/portada.tsx
+++ components/portada.tsx
@@ -84,15 +84,15 @@ const Portada: FC<PortadaProps> = ({ moviesProps }) => {
     }
   };
 
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(() => {
-  //     setFocusMovie((prevFocus) => (prevFocus + 1) % 5);
-  //   }, 5000);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFocusMovie((prevFocus) => (prevFocus + 1) % 5);
+    }, 5000);
 
-  //   return () => {
-  //     clearTimeout(timeoutId);
-  //   };
-  // }, [focusMovie]);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [focusMovie]);
 
   useEffect(() => {
     setMovieMain(dataMovies[focusMovie]);
